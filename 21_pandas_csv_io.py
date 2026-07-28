ec3b05fcad011bfe0bfc0e25c160192463753794
raniarashid780-sketch <raniarashid780@gmail.com>
2026-07-28 00:58:44 -0700

    Fix 21_pandas_csv_io.py: correct .info() print call, rename df2, add version check

--- 21_pandas_csv_io.py
+++ 21_pandas_csv_io.py
@@ -1,19 +1,19 @@
-"""Day 2: Pandas CSV I/O"""
+"""Day 2: Pandas CSV I/O."""
 import pandas as pd
+
 df = pd.read_csv("sample_sales.csv")
-# By default head gives data on first five rows
+
+# Prediction: head() shows first 5 rows, unformatted by my expectations of order
 print(df.head())
-# it will show the top 2 rows index starts from 0 so it will show rows 0 and 1
 print(df.head(2))
-# it will  show the full information about data
+
 df.info()
-# It will provide the mean, std, min, max etc of all data
-# by default it gives data on just numeric values
+
 print(df.describe())
-# if you include all it will give data on all datatypes
 print(df.describe(include="all"))
+
 df.to_csv("output.csv", index=False)
-df2 = pd.read_csv("output.csv")
-print(df.equals(df2))
+reloaded_df = pd.read_csv("output.csv")
+print(df.equals(reloaded_df))
 
-
+print(pd.__version__)
