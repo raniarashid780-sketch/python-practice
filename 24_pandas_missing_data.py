d2a201ea4b5812c4200a04b012c22367d2677396
raniarashid780-sketch <raniarashid780@gmail.com>
2026-07-30 17:21:48 +0500

    Fix the mistake of emoty value in revenue column so it will not be changed to object type.

--- 24_pandas_missing_data.py
+++ 24_pandas_missing_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 sample_data = pd.DataFrame({
     "Order_ID":["A-1042", "B-2087", "C-3114", "D-4201", "E-5126","F-6359"],
     "Department":["Electronics", "Home", "Sports", pd.NA, "Home", "Sports"],
-    "Revenue":[1250.5, 980.0, pd.NA, 300.75, 1300.50, 500.25],
+    "Revenue":[1250.5, 980.0, np.nan, 300.75, 1300.50, 500.25],
     "Region":["North", "South", "East", "West", "North", "West"]
 })
 # we are using numpy here because if we give an empty value as pd.NA in the Revenue column pandas will mark the column dtype as object so we are using numpy's empty value.
